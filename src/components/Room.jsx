16219f029e18304a963c126d76adecdb7e3577c8
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-07-13 16:52:59 +0500

    Added CSS

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,15 +1,19 @@
 import React,{useState} from 'react';
 
-
 function Room(props){
     const [isLit,setLit]=useState(false);
     const [temp,setTemp]=useState(32);
-
+    const [color,setColor]=useState("black");
+    const [textColor,setTextColor]=useState("white");
     function turnOn(){
         setLit(true);
+        setColor("white")
+        setTextColor("black");
     }
     function turnOff(){
         setLit(false);
+        setColor("black")
+        setTextColor("white");
     }
     function incTemp(){
         setTemp(temp+1);
@@ -18,7 +22,7 @@ function Room(props){
         setTemp(temp-1);
     }
 
-    return <div className="room"> <h1>The room is {isLit? "lit":"dark"}</h1>
+    return <div className="room" style={{backgroundColor:color,color:textColor }}> <h1>The room is {isLit? "lit":"dark"}</h1>
     <button onClick={turnOn}>On</button>
     <button onClick={turnOff}>Off</button>
     <p>Temprature={temp}℃</p>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import Room from './components/Room';
+import Room from './components/Room'
 import Reddit from './components/Reddit';
 
 ReactDOM.render(
